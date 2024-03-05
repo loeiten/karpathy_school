327cea23171195ccef87932bc78ffe49a3ac0962
Michael Løiten <michael.l.magnussen@gmail.com>
2024-03-05 12:02:39 +0100

    Add vocab

--- makemore_mlp/makemore_mlp/__init__.py
+++ makemore_mlp/makemore_mlp/__init__.py
@@ -1,1 +1,9 @@
 """Package containing makemore_mlp."""
+
+from typing import Literal
+
+# +1 as we'd like the start and stop token to have value 0
+TOKEN_TO_INDEX = {chr(ord("a") + i): i + 1 for i in range(26)}
+# Start/stop token
+TOKEN_TO_INDEX["."] = 0
+INDEX_TO_TOKEN = {value: key for key, value in TOKEN_TO_INDEX.items()}
