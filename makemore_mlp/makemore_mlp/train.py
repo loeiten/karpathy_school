7e26b1c18726da42823fe49924b38b7ce8eff152
Michael Løiten <michael.l.magnussen@gmail.com>
2024-03-15 14:12:20 +0100

    Checkpoint: Complete the loss description

--- makemore_mlp/makemore_mlp/train.py
+++ makemore_mlp/makemore_mlp/train.py
@@ -6,7 +6,8 @@ import torch
 
 def train_neural_net_model(
     model: Tuple[torch.Tensor, ...],
-    training_data: torch.Tensor,
+    input_data: torch.Tensor,
+    ground_truth_data: torch.Tensor,
     n_mini_batches: int,
     batch_size: int
 ) -> Tuple[torch.Tensor]:
@@ -14,7 +15,10 @@ def train_neural_net_model(
 
     Args:
         model (Tuple[torch.Tensor, ...]): The model (weights) to use
-        training_data (torch.Tensor): The training data
+        input_data (torch.Tensor): The input training data (the data fed into the
+            features)
+        ground_truth_data (torch.Tensor): The correct prediction for the input 
+            (the correct labels)
         n_mini_batches (int): Number of mini batches to use
         batch_size (int): The batch size to use
 
@@ -26,20 +30,20 @@ def train_neural_net_model(
 
     for _ in range(n_mini_batches):
         # Mini batch constructor
-        n_samples = training_data.shape[0]
+        n_samples = input_data.shape[0]
         idxs = torch.randint(low=0, high=n_samples, size=batch_size)
 
-        # NOTE: training_data has dimension (n_samples, block_size)
-        #       training_data[idxs] selects batch_size samples from the training 
+        # NOTE: input_data has dimension (n_samples, block_size)
+        #       input_data[idxs] selects batch_size samples from the training 
         #       data
-        #       The size of training_data[idxs] is therefore 
+        #       The size of input_data[idxs] is therefore 
         #       (batch_size, block_size)
         #       c has dimension (VOCAB_SIZE, embedding_size)
-        #       c[training_data[idxs]] will grab embedding_size vectors
+        #       c[input_data[idxs]] will grab embedding_size vectors
         #       for each of the block_size characters
         #       The dimension of emb is therefore 
         #       (batch_size, block_size, embedding_size)
-        emb = c[training_data[idxs]]
+        emb = c[input_data[idxs]]
         # NOTE: Given a block_size of 3 and an embedding size of 2, we could have 
         #       done the following:
         #
@@ -70,5 +74,26 @@ def train_neural_net_model(
         # NOTE: + b1 is broadcasting on the correct dimension
         # NOTE: The broadcasting will succeed
         h = torch.tanh(emb.view(-1, concatenated_dimension_size) @ w1 + b1)
+        # The logits will have dimension (batch_size, VOCAB_SIZE)
+        logits = h@w2 + b2
+        # Get the fake counts (like in makemore_bigram)
+        counts = logits.exp()
+        # Normalize to probability
+        prob = counts/counts.sum(1, keepdim=True)
+
+        # Negative loss likelihood:
+        # - For each of the example in the batch: torch.arange(batch_size)
+        #   - Select the probability of getting the ground truth:
+        #     prob[torch.arange(batch_size), ground_truth_data]
+        #     The shape of this will be the batch_size
+        #     In a model that perfectly predicts the characters all entries in
+        #     prob[torch.arange(batch_size), ground_truth_data]
+        #     would be 1
+        # - We take the logarithm of this, so that numbers under 1 becomes
+        #   negative (in perfect prediction log(1)=0)
+        #   - In order to get a positive number to minimize we take the negative
+        #     of the result
+        # - Finally, the mean of this the number we want to minimize
+        loss = -prob[torch.arange(batch_size), ground_truth_data].log().mean()
 
     return c, w1, b1, w2, b2
