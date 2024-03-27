2bb22b5e3629423dbc5ef5c16340f914cb1491de
Michael Løiten <michael.l.magnussen@gmail.com>
2024-03-27 10:54:48 +0100

    Add loss and step counters

--- makemore_mlp/makemore_mlp/train.py
+++ makemore_mlp/makemore_mlp/train.py
@@ -1,6 +1,6 @@
 """Module to train the model."""
 
-from typing import Optional, Tuple
+from typing import List, Optional, Tuple
 
 import torch
 import torch.nn.functional as F
@@ -13,7 +13,7 @@ def train_neural_net_model(
     input_training_data: torch.Tensor,
     ground_truth_data: torch.Tensor,
     model_options: Optional[ModelOptions],
-) -> Tuple[torch.Tensor]:
+) -> Tuple[Tuple[torch.Tensor], List[float], List[int]]:
     """Train the neural net model.
 
     Args:
@@ -27,24 +27,25 @@ def train_neural_net_model(
 
     Returns:
         Tuple[torch.Tensor, ...]: The trained model
+        List[float]: The log10 of the loss for each step
+        List[int]: The step
     """
     if model_options is None:
         model_options = ModelOptions()
-    n_mini_batches = model_options.n_mini_batches
-    batch_size = model_options.batch_size
-    learning_rate = model_options.learning_rate
 
     # Make it possible to train
     for parameters in model:
         parameters.requires_grad = True
 
+    step = []
+    loss_log_10 = []
     # NOTE: It's better to take a lot of steps in the approximate direction of
     #       the true gradient than it is to take one big step in the direction
     #       of the true gradient
-    for _ in range(n_mini_batches):
+    for i in range(model_options.n_mini_batches):
         # Mini batch constructor
         n_samples = input_training_data.shape[0]
-        idxs = torch.randint(low=0, high=n_samples, size=batch_size)
+        idxs = torch.randint(low=0, high=n_samples, size=model_options.batch_size)
 
         # NOTE: input_training_data has dimension (n_samples, block_size)
         #       input_training_data[idxs] selects batch_size samples from the
@@ -75,12 +76,20 @@ def train_neural_net_model(
         # #     negative
         # #     of the result
         # # - The mean of this the number we want to minimize
-        # loss = -prob[torch.arange(batch_size), ground_truth_data].log().mean()
+        # loss = (
+        #     -prob[torch.arange(model_options.batch_size), ground_truth_data]
+        #     .log()
+        #     .mean()
+        # )
 
         # However, it's much more efficient both mathematically and in terms of
         # backprop
         # It's also numerically better behaved
         loss = F.cross_entropy(logits, ground_truth_data)
+
+        # Append loss and iteration
+        loss_log_10.append(loss.log10().item())
+        step.append(i)
 
         # Backward pass
 
@@ -92,6 +101,6 @@ def train_neural_net_model(
 
         # Update the weights
         for parameters in model:
-            parameters.data += -learning_rate * parameters.grad
+            parameters.data += -model_options.learning_rate * parameters.grad
 
-    return model
+    return model, loss_log_10, step
