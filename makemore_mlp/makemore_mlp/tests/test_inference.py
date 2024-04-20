2b2278be83993a62409de8081e00bdf2fd0731eb
Michael Løiten <michael.l.magnussen@gmail.com>
2024-04-20 12:16:21 +0200

    Add test to parse for inference

--- makemore_mlp/makemore_mlp/tests/test_inference.py
+++ makemore_mlp/makemore_mlp/tests/test_inference.py
@@ -1,6 +1,6 @@
 """Contains tests for the inference module."""
 
-from makemore_mlp.inference import run_inference
+from makemore_mlp.inference import parse_args, run_inference
 from makemore_mlp.models import get_model
 
 
@@ -11,3 +11,22 @@ def test_run_inference() -> None:
     # Run inference on the untrained model
     predictions = run_inference(model=model, n_samples=2)
     assert len(predictions) == 2
+
+
+def test_parse_args() -> None:
+    """Test that the arg parsing works"""
+    # Test the long arguments
+    n_predictions = 2
+    long_arguments = ["--n-predictions", f"{n_predictions}"]
+    args = parse_args(long_arguments)
+    assert args.n_predictions == n_predictions
+
+    short_arguments = []
+    for arg in long_arguments:
+        if arg.startswith("--"):
+            short_arguments.append(arg[1:3])
+        else:
+            short_arguments.append(arg)
+
+    args = parse_args(short_arguments)
+    assert args.n_predictions == n_predictions
