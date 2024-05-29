4c6f1a6e9e6cc775ea7be805c7405f86b4b72823
Michael Løiten <michael.l.magnussen@gmail.com>
2024-05-29 12:37:53 +0200

    Add argparse test dist diag

--- makemore_agb/makemore_agb/tests/test_distribution_diagnostics.py
+++ makemore_agb/makemore_agb/tests/test_distribution_diagnostics.py
@@ -1,7 +1,7 @@
 """Contains tests for the distribution diagnostics module."""
 
 import pytest
-from makemore_agb.distribution_diagnostics import plot_initial_distributions
+from makemore_agb.distribution_diagnostics import parse_args, plot_initial_distributions
 
 
 @pytest.mark.parametrize("good_initialization", [True, False])
@@ -13,3 +13,30 @@ def test_plot_initial_distributions(good_initialization: bool) -> None:
             for the initialization weights
     """
     plot_initial_distributions(good_initialization=good_initialization, show=False)
+
+
+def test_parse_args() -> None:
+    """Test that the arg parsing works"""
+    # Test the long arguments
+    ground_truth = {
+        "--good-initialization": True,
+        "--batch-normalize": True,
+    }
+    long_short_map = {
+        "--good-initialization": "-g",
+        "--batch-normalize": "-m",
+    }
+    arguments = list(ground_truth.keys())
+    args = parse_args(arguments)
+    parsed_map = {
+        "--good-initialization": args.good_initialization,
+        "--batch-normalize": args.batch_normalize,
+    }
+
+    for arg, val in ground_truth.items():
+        assert val == parsed_map[arg]
+
+    arguments = list(long_short_map.values())
+    args = parse_args(arguments)
+    for arg, val in ground_truth.items():
+        assert val == parsed_map[arg]
