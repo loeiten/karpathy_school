97e955cfe8a6a10f60f57527ca86eedffb595bda
Michael Løiten <michael.l.magnussen@gmail.com>
2024-06-04 16:52:26 +0200

    Add tanh layer

--- makemore_agb/makemore_agb/models.py
+++ makemore_agb/makemore_agb/models.py
@@ -133,7 +133,7 @@ def get_explicit_model(
 
 
 class Linear:
-    """Class mimicking the torch.nn.Linear module in PyTorch."""
+    """Class mimicking the torch.nn.Linear Module in PyTorch."""
 
     # Reducing the number of arguments here would be counter productive
     # pylint: disable-next=too-many-arguments
@@ -159,7 +159,7 @@ class Linear:
         g = torch.Generator(device=device).manual_seed(seed)
         self.weight = torch.randn((fan_in, fan_out), generator=g) / fan_in**0.5
         self.bias = torch.zeros(fan_out) if bias else None
-        # NOTE: This is not available in the original torch layer, but is added
+        # NOTE: This is not implemented in the original torch layer, but is added
         #       for plotting convenience
         self.out = torch.empty(fan_out, device=device)
 
@@ -187,3 +187,42 @@ class Linear:
         if self.bias is not None:
             params.append(self.bias)
         return params
+
+
+class Tanh:
+    """Class mimicking the torch.nn.Tanh Module in PyTorch."""
+
+    # Reducing the number of arguments here would be counter productive
+    # pylint: disable-next=too-many-arguments
+    def __init__(
+        self,
+        device: Optional[torch.device] = None,
+    ):
+        """Initialize the output for the tanh layer.
+
+        Args:
+            device (Optional[torch.device], optional): Device to use for the tensors.
+        """
+        # NOTE: This is not implemented in the original torch layer, but is added
+        #       for plotting convenience
+        self.out = torch.empty(0, device=device)
+
+    def __call__(self, x: torch.Tensor) -> torch.Tensor:
+        """Return the output of the layer given on the input.
+
+        Args:
+            x (torch.Tensor): The input tensor
+
+        Returns:
+            torch.Tensor: The output tensor
+        """
+        self.out = torch.tanh(x)
+        return self.out
+
+    def parameters(self) -> List[torch.Tensor]:
+        """Return the parameters.
+
+        Returns:
+            List[torch.Tensor]: The parameters of the layer
+        """
+        return []
