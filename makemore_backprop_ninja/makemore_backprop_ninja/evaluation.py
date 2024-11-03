55f17b0768ab075af82f3122b0739cf30e805f60
Michael Løiten <michael.l.magnussen@gmail.com>
2024-11-03 10:52:04 +0100

    Docstring fix

--- makemore_backprop_ninja/makemore_backprop_ninja/evaluation.py
+++ makemore_backprop_ninja/makemore_backprop_ninja/evaluation.py
@@ -20,7 +20,7 @@ def evaluate(
         model (Tuple[torch.Tensor, ...]): The model
         input_data (torch.Tensor): The data to do the prediction on
         batch_normalization_parameters (BatchNormalizationParameters):
-            If set: Contains the running mean and the running standard deviation
+            Contains the running mean and the running standard deviation
         ground_truth (torch.Tensor): The ground truth of the predictions
 
     Returns:
