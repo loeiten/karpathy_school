b23ec1c2c72e9d8177f9af9d788b4ce9ac7d09db
Michael Løiten <michael.l.magnussen@gmail.com>
2024-12-22 21:14:15 +0100

    Elaborate on the explanation

--- makemore_backprop_ninja/makemore_backprop_ninja/train.py
+++ makemore_backprop_ninja/makemore_backprop_ninja/train.py
@@ -332,7 +332,8 @@ def manual_backprop(
     # However, we don't know dl/d(counts_sum yet, but it can be calculated
     # Do let's calculate that first
     # 3. dl/d(counts_sum) = dl/d(counts_sum_inv) * d(counts_sum_inv)/d(counts_sum)
-    # dl/d(counts_sum) = dl / dx * dx / d(counts_sum)
+    #    Since d/dx (1/x) = -1/(x^2), we get
+    #    dl/d(counts_sum) = dl/d(counts_sum_inv) * (-1/counts^2)
     dl_d_counts_sum = torch.zeros_like(counts_sum)
     dl_d_counts = torch.zeros_like(counts)
     dl_d_normalized_logits = torch.zeros_like(normalized_logits)
