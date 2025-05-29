938bbdbcf180f10c495b39ec2ca0bf1f5e6ba6af
Michael Løiten <michael.l.magnussen@gmail.com>
2025-05-29 20:37:50 +0200

    Add succinct to dataclasses

--- makemore_backprop_ninja/makemore_backprop_ninja/backprop_helpers/dataclasses.py
+++ makemore_backprop_ninja/makemore_backprop_ninja/backprop_helpers/dataclasses.py
@@ -8,8 +8,10 @@ class BackpropMode(Enum):
     Contains backprop modes.
 
     AUTOMATIC - Uses pytorch autograd
-    VERBOSE - Calculate the gradients by hand in an verbose way
+    VERBOSE - Calculate the gradients by hand in a verbose way
+    SUCCINCT - Calculate the gradients by hand in a succinct way
     """
 
     AUTOMATIC = "AUTOMATIC"
     VERBOSE = "VERBOSE"
+    SUCCINCT = "SUCCINCT"
