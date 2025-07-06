9575ec21797eb17277550c4a634a645e848f79c0
Michael Løiten <michael.l.magnussen@gmail.com>
2025-07-06 16:19:46 +0200

    Test encoder

--- 6_gpt_from_scratch/gpt_from_scratch/tests/test_preprocessing.py
+++ 6_gpt_from_scratch/gpt_from_scratch/tests/test_preprocessing.py
@@ -23,3 +23,17 @@ def test_CharTokenizer__init__(qwerty_tokenizer: CharTokenizer) -> None:
     # NOTE: sort sorts according to their ASCII values
     assert qwerty_tokenizer.tokens == ["E", "T", "e", "q", "r", "w", "y"]
     assert qwerty_tokenizer.vocab_size == 7
+
+
+def test_CharTokenizer__encode__(qwerty_tokenizer: CharTokenizer) -> None:
+    """Test the CharTokenizer encoder.
+
+    Args:
+        qwerty_tokenizer (CharTokenizer): The qweErTy tokenizer
+    """
+    assert qwerty_tokenizer.encode("ETeqrwy") == [0, 1, 2, 3, 4, 5, 6]
+
+    with pytest.raises(KeyError):
+        qwerty_tokenizer.encode("a")
+
+    assert qwerty_tokenizer.encode("weTTer") == [5, 2, 1, 1, 2, 4]
